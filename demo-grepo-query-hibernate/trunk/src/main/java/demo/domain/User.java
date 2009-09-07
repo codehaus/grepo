194991ea6924a26de1af5f0f11df2a8629007a15
dguggi <dguggi@729f7ef0-7c71-0410-bb00-e2a08ff96a94>
2009-09-07 13:58:34 +0000

    upd format

--- demo-grepo-query-hibernate/trunk/src/main/java/demo/domain/User.java
+++ demo-grepo-query-hibernate/trunk/src/main/java/demo/domain/User.java
@@ -40,7 +40,6 @@ public class User {
     /** The email property. */
     private String email;
 
-
     public Long getId() {
         return id;
     }
@@ -50,41 +49,33 @@ public class User {
         this.id = id;
     }
 
-
     public String getFirstname() {
         return firstname;
     }
-
 
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
-
     public String getLastname() {
         return lastname;
     }
-
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-
     public String getUsername() {
         return username;
     }
-
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-
     public String getEmail() {
         return email;
     }
-
 
     public void setEmail(String email) {
         this.email = email;
