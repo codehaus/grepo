27c4bdb8c9fbe1a615003c96a02989df714c0d1a
dguggi <dguggi@729f7ef0-7c71-0410-bb00-e2a08ff96a94>
2010-05-03 21:36:22 +0000

    added tests for exception translation

---
+++ grepo/grepo-query-hibernate/src/test/java/org/codehaus/grepo/query/hibernate/repository/ExceptionTranslationTestRepository1.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010 Grepo Committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.grepo.query.hibernate.repository;
+
+import org.codehaus.grepo.query.commons.annotation.GenericQuery;
+import org.codehaus.grepo.query.commons.repository.GenericQueryRepository;
+import org.codehaus.grepo.query.hibernate.TestEntity;
+
+/**
+ * @author dguggi
+ */
+public interface ExceptionTranslationTestRepository1  extends GenericQueryRepository<TestEntity> {
+    /**
+     * @param firstName The first name.
+     * @return Returns the test entity.
+     */
+    @GenericQuery(query = "from TestEntity where firstname = ?")
+    TestEntity getByFirstName(String firstName);
+}
