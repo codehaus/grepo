479aeb09b6e067df108ed7407f85d5f841fa619d
dguggi <dguggi@729f7ef0-7c71-0410-bb00-e2a08ff96a94>
2010-07-07 13:03:42 +0000

    upd

---
+++ grepo/grepo-statistics/src/main/java/org/codehaus/grepo/statistics/service/StatisticsControllerImpl.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2010 Grepo Committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.grepo.statistics.service;
+
+import org.codehaus.grepo.statistics.collection.StatisticsCollection;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+/**
+ * @author dguggi
+ */
+@ManagedResource("Statistics controller")
+public class StatisticsControllerImpl implements StatisticsController {
+
+    /** The statistics manager. */
+    private StatisticsManager statisticsManager;
+
+    /** The collection. */
+    private StatisticsCollection statisticsCollection;
+
+    /**
+     * {@inheritDoc}
+     */
+    @ManagedAttribute(description = "Enables/Disables statistics")
+    public void setStatisticsEnabled(boolean statisticsEnabled) {
+        statisticsManager.setEnabled(statisticsEnabled);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @ManagedAttribute
+    public boolean getStatisticsEnabled() {
+        return statisticsManager.isEnabled();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @ManagedAttribute(description = "Sets the max number of statistics entries")
+    public void setMaxStatisticsEntries(Long maxStatisticsEntries) {
+        statisticsCollection.setMaxStatisticsEntries(maxStatisticsEntries);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @ManagedAttribute
+    public Long getMaxStatisticsEntries() {
+        return statisticsCollection.getMaxStatisticsEntries();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @ManagedOperation(description = "Clears statistics")
+    public void clearStatistics() {
+        statisticsCollection.clear();
+    }
+
+    public void setStatisticsManager(StatisticsManager statisticsManager) {
+        this.statisticsManager = statisticsManager;
+    }
+
+    public void setStatisticsCollection(StatisticsCollection statisticsCollection) {
+        this.statisticsCollection = statisticsCollection;
+    }
+
+}
