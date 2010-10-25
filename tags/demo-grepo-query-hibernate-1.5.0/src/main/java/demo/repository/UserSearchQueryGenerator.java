451524f718b75cc01e1add179cdd60753e15a273
dguggi <dguggi@729f7ef0-7c71-0410-bb00-e2a08ff96a94>
2010-10-25 20:15:21 +0000

    demo-grepo-query-hibernate 1.5.0

---
+++ tags/demo-grepo-query-hibernate-1.5.0/src/main/java/demo/repository/UserSearchQueryGenerator.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2009 Grepo Committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package demo.repository;
+
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.grepo.query.commons.aop.QueryMethodParameterInfo;
+import org.codehaus.grepo.query.hibernate.generator.AbstractHibernateQueryGenerator;
+
+/**
+ * @author dguggi
+ */
+public class UserSearchQueryGenerator extends AbstractHibernateQueryGenerator {
+
+    /**
+     * {@inheritDoc}
+     */
+    public String generate(QueryMethodParameterInfo qmpi) {
+        String firstname = qmpi.getParameterByParamName("fn", String.class);
+        String lastname = qmpi.getParameterByParamName("ln", String.class);
+
+        StringBuilder query = new StringBuilder("from User where 1=1");
+        if (StringUtils.isNotEmpty(firstname)) {
+            query.append(" AND firstname = :fn");
+        }
+        if (StringUtils.isNotEmpty(lastname)) {
+            query.append(" AND lastname = :ln");
+        }
+
+        return query.toString();
+    }
+
+}
